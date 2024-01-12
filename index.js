191a3498e7aa6f2d3e5660c55570a97a6f63417e
angelica-zach <angelscakeunicorn@gmail.com>
2024-01-12 16:55:17 +0000

    added questions

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const generateMarkdown = require("./starter/utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install this?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'If you created an application or package and would like other developers to contribute it,add guidelines for how to do so',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: ' Provide examples on how to run tests',
+      },
+      {
+        type: 'list',
+        name: 'liscence',
+        message: 'Pick a liscence',
+      },
+     
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
+  
 ];
 
 // function to write README file
